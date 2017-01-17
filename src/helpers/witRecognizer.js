7c9fdfe151eedd8c2aac0d16a7b6d221c21afbe3
Sebastian <sebastian@furonto.com>
2017-01-17 19:06:21 +0100

    add config.json import

--- src/helpers/witRecognizer.js
+++ src/helpers/witRecognizer.js
@@ -1,4 +1,5 @@
 const WitRecognizer = require('botbuilder-wit');
+const config = require('../../config');
 
 exports.witRecognizer = new WitRecognizer(process.env.WIT_ACCESS_TOKEN || config.WIT_ACCESS_TOKEN);
 exports.witClient = exports.witRecognizer.witClient;
