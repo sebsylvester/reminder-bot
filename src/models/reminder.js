aee6f3df495828b0ea4339b9e39d0350012dd2d3
Sebastian <sebastian@furonto.com>
2017-02-16 20:37:09 +0100

    Improve mongoose schema, index user.name

--- src/models/reminder.js
+++ src/models/reminder.js
@@ -1,7 +1,24 @@
 const mongoose = require('mongoose');
 
 const reminderSchema = new mongoose.Schema({
-    user_address: { type: Object, required: true, index: true },
+    user_address: {
+        useAuth: Boolean,
+        serviceUrl: String,
+        bot: { 
+            name: String, 
+            id: String 
+        },
+        conversation: { 
+            id: String, 
+            isGroup: Boolean 
+        },
+        user: { 
+            name: { type: String, index: true }, 
+            id: String 
+        },
+        channelId: String,
+        id: String
+    },
     value: { type: String, required: true },
     expiration: { type: Date, required: true }
 });
