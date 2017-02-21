b1ea9d79256e1d1fe856fe1b088d6ebbedf697bc
Sebastian <sebastian@furonto.com>
2017-02-21 10:55:56 +0100

    update to reflect the changes in botbuilder-wit

--- src/helpers/witRecognizer.js
+++ src/helpers/witRecognizer.js
@@ -1,4 +1,4 @@
-const WitRecognizer = require('botbuilder-wit');
+const { WitRecognizer } = require('botbuilder-wit');
 const config = require('../../config');
 
 exports.witRecognizer = new WitRecognizer(process.env.WIT_ACCESS_TOKEN || config.WIT_ACCESS_TOKEN);
